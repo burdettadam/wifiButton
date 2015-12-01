887e0e7241dfd070827b064021dbfda1bfe654b6
Adam Burdett <burdettadam@gmail.com>
2015-11-30 18:17:36 -0700

    two buttons

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 var config = require('./config'),
     dash_button = require('node-dash-button'),
-    dash = dash_button(config.button.id);
+    dash = dash_button([config.Tide.id,config.Izze.id]);
     request = require('request');
 
-dash.on("detected", function (){
+dash.on("detected", function (dash_id){
    var message = {
         Attributes: {
-          "address" : config.button.id
+          "address" : dash_id
         }
       };
     sendToKRE(message);
     console.log("Ding");
-  });
+});
 
 var sendToKRE = function(message){
   var url = config.KRE.webhook;
